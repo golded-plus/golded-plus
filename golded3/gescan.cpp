e44ee6a11338c80c5f0c14e13a41353bcbefec11
Alexander S. Aganichev <asa@eed.miee.ru>
2000-12-19 10:19:20 +0000

    Fix crash at startup when scan enabled

--- golded3/gescan.cpp
+++ golded3/gescan.cpp
@@ -108,7 +108,7 @@ int AreaList::SetActiveAreaNo(int __areano) {
 int AreaList::AreaScan(int mode, uint currno, int pmscan, int& pmails, int& pmareas, const char* file) {
 
   // Never scan if there's active area
-  if(AA->isopen())
+  if(AA and AA->isopen())
     return false;
 
   gstrarray bag;
