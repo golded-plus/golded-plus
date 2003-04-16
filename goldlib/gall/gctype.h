b45a762d2e7acd551debc22b69ca97d3064aee60
Alexander S. Aganichev <asa@eed.miee.ru>
2003-04-16 08:38:17 +0000

    Fix warnings

--- goldlib/gall/gctype.h
+++ goldlib/gall/gctype.h
@@ -1,39 +1,37 @@
-//  This may look like C code, but it is really -*- C++ -*-
-
-//  ------------------------------------------------------------------
-//  The Goldware Library
-//  Copyright (C) 1999 Alexander S. Aganichev
-//  ------------------------------------------------------------------
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Library General Public
-//  License as published by the Free Software Foundation; either
-//  version 2 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Library General Public License for more details.
-//
-//  You should have received a copy of the GNU Library General Public
-//  License along with this program; if not, write to the Free
-//  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-//  MA 02111-1307, USA
-//  ------------------------------------------------------------------
-//  $Id$
-//  ------------------------------------------------------------------
-//  Portable NLS functions for ctype.
-//  ------------------------------------------------------------------
+/*  ------------------------------------------------------------------
+ *  The Goldware Library
+ *  Copyright (C) 1999 Alexander S. Aganichev
+ *  ------------------------------------------------------------------
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA 02111-1307, USA
+ *  ------------------------------------------------------------------
+ *  $Id$
+ *  ------------------------------------------------------------------
+ *  Portable NLS functions for ctype.
+ *  --------------------------------------------------------------- */
 
 #ifndef __gctype_h
 #define __gctype_h
 
 
-//  ------------------------------------------------------------------
+/*  --------------------------------------------------------------- */
 
 #include <gcmpall.h>
 
 
-//  ------------------------------------------------------------------
+/*  --------------------------------------------------------------- */
 
 #ifdef __BORLANDC__
 #define __USELOCALES__
@@ -60,29 +58,34 @@ extern char tl[256], tu[256];
 #endif
 
 
-//  ------------------------------------------------------------------
+/*  --------------------------------------------------------------- */
 
-// NLS chars detected by converting to lower or upper case and in case they don't match they treated as characters
+/* NLS chars detected by converting to lower or upper case and in case
+ * they don't match they treated as characters
+ */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern __inline__ int isxalnum(char c) { return isascii(c) ? isalnum(c) : (c != tolower(c)) || (c != toupper(c)); }
+extern __inline__ int isxalnum(char c) {
+  return isascii(c) ? isalnum(c) : (c != tolower(c)) || (c != toupper(c));
+}
 #ifdef __cplusplus
 }
 #endif
 
 #ifdef __BEOS__
-//sz: there are some problems under BeOS with that function - symbols
-// from second half of ASCII table are assumed as control ones ...
-// This is a real disaster for cyrillic users ...
-// It's also not possible to use setlocale() to change it's behaviour. =-(
+/* sz: there are some problems under BeOS with that function - symbols
+ * from second half of ASCII table are assumed as control ones ...
+ * This is a real disaster for cyrillic users ...
+ * It's also not possible to use setlocale() to change it's behaviour. =-(
+ */
 #undef iscntrl
 # define iscntrl(c)	((c < 0x7f) ? __isctype((c), _IScntrl) : 0)
 #endif //__BEOS__
 
-//  ------------------------------------------------------------------
+/*  --------------------------------------------------------------- */
 
 #endif
 
-//  ------------------------------------------------------------------
+/*  --------------------------------------------------------------- */
