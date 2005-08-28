77ec3e74c65237ebc256ed9531e2ec09ff07f50c
Stas Degteff <stas_degteff@users.sourceforge.net>
2005-08-28 19:24:46 +0000

    Microsoft Visual Studio build for rddt

--- golded3/geglob.h
+++ golded3/geglob.h
@@ -2,7 +2,8 @@
 //  ------------------------------------------------------------------
 //  GoldED+
 //  Copyright (C) 1990-1999 Odinn Sorensen
-//  Copyright (C) 1999-2000 Alexander S. Aganichev
+//  Copyright (C) 1999-2004 Alexander S. Aganichev
+//  Copyright (C) 2005 Stas Degteff
 //  ------------------------------------------------------------------
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License as
@@ -18,12 +19,19 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 //  MA 02111-1307 USA
+//
+//  See also http://www.gnu.org
 //  ------------------------------------------------------------------
 //  $Id$
 //  ------------------------------------------------------------------
 //  Global data.
 //  ------------------------------------------------------------------
 
+#ifndef __GEGLOB_H
+#define __GEGLOB_H
+
+//  Golded+ version information --------------------------------------
+#include "golded3.h"
 
 //  ------------------------------------------------------------------
 
@@ -154,36 +162,5 @@ extern int blanked;
 
 extern int startupscan_success;
 
-
 //  ------------------------------------------------------------------
-//  Global compile constants
-
-extern const char* __gver_vendor_name__;
-extern const char* __gver_vendor_fido__;
-extern const char* __gver_vendor_email__;
-extern const char* __gver_prename__;
-extern const char* __gver_preshortname__;
-extern const char* __gver_name__;
-extern const char* __gver_shortname__;
-extern const char* __gver_shortlogname__;
-extern const char* __gver_postshortname__;
-extern const char* __gver_postname__;
-extern const char* __gver_platform__;
-extern const char* __gver_shortplatform__;
-extern const char* __gver_cfgext__;
-extern const char* __gver_preversion__;
-extern const char* __gver_postversion__;
-extern int         __gver_major__;
-extern int         __gver_minor__;
-extern int         __gver_release__;
-extern const char* __gver_releasename__;
-extern const char* __gver_date__;
-extern const char* __gver_time__;
-extern const char* __gver_longpid__;
-extern const char* __gver_shortpid__;
-extern const char* __gver_ver__;
-extern const char* __gver_shortver__;
-
-
-//  ------------------------------------------------------------------
-
+#endif
---
+++ golded3/golded3.h
@@ -0,0 +1,62 @@
+
+//  ------------------------------------------------------------------
+//  GoldED+
+//  Copyright (C) 1990-1999 Odinn Sorensen
+//  Copyright (C) 1999-2004 Alexander S. Aganichev
+//  Copyright (C) 2005 Stas Degteff
+//  ------------------------------------------------------------------
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License as
+//  published by the Free Software Foundation; either version 2 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+//  MA 02111-1307 USA
+//
+//  See also http://www.gnu.org
+//  ------------------------------------------------------------------
+//  $Id$
+//  ------------------------------------------------------------------
+//  Global data: Golded+ version information.
+//  ------------------------------------------------------------------
+
+#ifndef __GOLDED3_H
+#define __GOLDED3_H
+
+//  ------------------------------------------------------------------
+//  Global compile constants
+
+extern const char* __gver_vendor_name__;
+extern const char* __gver_vendor_fido__;
+extern const char* __gver_vendor_email__;
+extern const char* __gver_prename__;
+extern const char* __gver_preshortname__;
+extern const char* __gver_name__;
+extern const char* __gver_shortname__;
+extern const char* __gver_shortlogname__;
+extern const char* __gver_postshortname__;
+extern const char* __gver_postname__;
+extern const char* __gver_platform__;
+extern const char* __gver_shortplatform__;
+extern const char* __gver_cfgext__;
+extern const char* __gver_preversion__;
+extern const char* __gver_postversion__;
+extern int         __gver_major__;
+extern int         __gver_minor__;
+extern int         __gver_release__;
+extern const char* __gver_releasename__;
+extern const char* __gver_date__;
+extern const char* __gver_time__;
+extern const char* __gver_longpid__;
+extern const char* __gver_shortpid__;
+extern const char* __gver_ver__;
+extern const char* __gver_shortver__;
+
+#endif
