367d6f986dcb6d1e6b84176a8b19cc8870296e2b
Stas Degteff <stas_degteff@users.sourceforge.net>
2005-11-06 12:50:23 +0000

    Script to build linux binaries

---
+++ dist-gpl.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Create a archive 'gpw32-*.zip' (gpwYMMDD.zip) with Win32 (MinGW) build
+# of the Golded+.
+
+date=`date +%Y%m%d`
+shortdate=`echo ${date} | sed s/^...//`
+shortname=../gpl${shortdate}.zip
+name=gpl115-${date}.tar.bz2
+
+sed -i.orig -e "s/\#define __GVER_POSTVERSION__ .*/\#define __GVER_POSTVERSION__   \"-b${date}\"/" golded3/mygolded.h
+
+files="bin/gedlnx.exe bin/gnlnx.exe bin/rddtlnx.exe"
+files="${files} docs/copying docs/copying.lib"
+files="${files} docs/golded.html docs/golded.txt docs/goldnode.html"
+files="${files} docs/goldnode.txt docs/license.txt docs/notework.txt"
+files="${files} docs/rddt.html docs/rddt.txt docs/readme.txt"
+files="${files} docs/rusfaq.txt docs/tips.txt docs/todowork.txt"
+files="${files} docs/tokencfg.txt docs/tokentpl.txt"
+
+printf 'GoldED+1.1.5 [`uname` binaries]\r\n'  >bin/File_ID.Diz
+printf 'Snapshot (development version)\r\n' >>bin/File_ID.Diz
+printf 'This is  unstable release  and\r\n' >>bin/File_ID.Diz
+printf 'it should be used for testing.\r\n' >>bin/File_ID.Diz
+printf '------------------------------\r\n' >>bin/File_ID.Diz
+printf 'GoldED+ is a  successor of the\r\n' >>bin/File_ID.Diz
+printf 'wellknown  GoldED mail editor.\r\n' >>bin/File_ID.Diz
+printf '------------------------------\r\n' >>bin/File_ID.Diz
+printf '*golded-plus.sourceforge.net* \r\n' >>bin/File_ID.Diz
+
+make PLATFORM=lnx clean
+make PLATFORM=lnx
+make PLATFORM=lnx strip
+make docs
+zip -9DXj ${name} bin/File_ID.Diz $files
+cp ${name} ${shortname}
