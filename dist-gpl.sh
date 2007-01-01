fbbd1f786900704f9dedda61dec473670c9e8d02
Stas Degteff <stas_degteff@users.sourceforge.net>
2007-01-01 19:02:29 +0000

    utf8 and koi8-r russian documentation

--- dist-gpl.sh
+++ dist-gpl.sh
@@ -22,8 +22,9 @@ files="${bines} docs/copying docs/copying.lib"
 files="${files} docs/golded.html docs/golded.txt docs/goldnode.html"
 files="${files} docs/goldnode.txt docs/license.txt docs/notework.txt"
 files="${files} docs/rddt.html docs/rddt.txt docs/readme.txt"
-files="${files} docs/rusfaq.txt docs/tips.txt docs/todowork.txt"
-files="${files} docs/tokencfg.txt docs/tokentpl.txt"
+files="${files} docs/rusfaq.txt docs/rusfaq.koi8r docs/rusfaq.utf8 docs/tips.txt docs/todowork.txt"
+files="${files} docs/tokencfg.txt docs/tokentpl.txt bin/requirements.txt"
+files="${files} docs/notework.rus docs/notework.koi8r docs/notework.utf8"
 
 printf "GoldED+1.1.5 [`uname` binaries]\r\n"  >bin/File_ID.Diz
 printf 'Snapshot (development version)\r\n' >>bin/File_ID.Diz
@@ -43,6 +44,15 @@ make docs
 for i in ${bines} ; do
   if [ ! -f ${i} ] ; then echo "File ${i} not exists, stop!"; exit 1 ; fi
 done
+
+iconv -c -f cp866 -t koi8-r docs/rusfaq.txt | sed 2s/cp866/koi8-r/ >docs/rusfaq.koi8r
+iconv -c -f cp866 -t utf8 docs/rusfaq.txt |  sed 2s/cp866/utf-8/ >docs/rusfaq.utf8
+
+iconv -c -f cp866 -t koi8-r docs/notework.rus | sed 2s/cp866/koi8-r/ >docs/notework.koi8r
+iconv -c -f cp866 -t utf8 docs/notework.rus |  sed 2s/cp866/utf-8/ >docs/notework.utf8
+
+echo Required libraries:  >bin/requirements.txt
+ldd bin/gedlnx | sed -e "s/ =.*//" -e /\\/lib\\//d -e "s/\\.so\\./ /" >>bin/requirements.txt
 
 zip -9DXj ${name} bin/File_ID.Diz $files
 cp ${name} ${shortname}
