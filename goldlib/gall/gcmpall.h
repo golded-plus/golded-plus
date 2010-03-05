1a3d650ba0b2b03f422e079611febfd1b98f0f5d
Stas Degteff <stas_degteff@users.sourceforge.net>
2010-03-05 14:17:48 +0000

    More accuracy platform-compiler definitions

--- goldlib/gall/gcmpall.h
+++ goldlib/gall/gcmpall.h
@@ -73,11 +73,17 @@
   #if defined(__OpenBSD__)
     #define __UNIX__ __OpenBSD__
   #endif
+  #if defined(__DragonFly__)
+    #define __UNIX__ __DragonFly__
+  #endif
   #if defined(__BEOS__)
     #define __UNIX__ __BEOS__
   #endif
   #if defined(__QNXNTO__)
     #define __UNIX__ __QNXNTO__
+  #endif
+  #if defined(__APPLE__)
+    #define __UNIX__ __APPLE__
   #endif
 #endif
 
