ba101fcb7c9d7e1bcc6144f82a52240e149e588d
Stas Degteff <stas_degteff@users.sourceforge.net>
2011-03-14 02:31:14 +0000

    License comments

--- goldlib/gall/gespell.h
+++ goldlib/gall/gespell.h
@@ -1,27 +1,30 @@
 
 //  ------------------------------------------------------------------
-//  GoldED+
+//  The Goldware Library
 //  Copyright (C) 1990-1999 Odinn Sorensen
 //  Copyright (C) 1999-2000 Alexander S. Aganichev
+//  Copyright (C) 2005-2006 Ianos Gnatius
+//  Copyright (C) 2011 Max Vasilyev
+//  Copyright (C) 2011 Stas Degteff
 //  ------------------------------------------------------------------
-//  This program is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU General Public License as
-//  published by the Free Software Foundation; either version 2 of the
-//  License, or (at your option) any later version.
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Library General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
 //
-//  This program is distributed in the hope that it will be useful,
+//  This library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  General Public License for more details.
+//  Library General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-//  MA 02111-1307 USA
+//  You should have received a copy of the GNU Library General Public
+//  License along with this program; if not, write to the Free
+//  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+//  MA 02111-1307, USA
 //  ------------------------------------------------------------------
 //  $Id$
 //  ------------------------------------------------------------------
-//  SpellChecker Structures and Classes.
+//  SpellChecker Structures and Classes declarations.
 //  ------------------------------------------------------------------
 
 #ifndef __GESPELL_H__
